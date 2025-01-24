86daa9b5f4e58d1f6c605c59e860d75f52e1cc31
Naman Soni <namantsoni@gmail.com>
2025-01-24 13:06:05 +0530

    Clerk Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
