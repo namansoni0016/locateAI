2af69255a4bbd60534166ee814007c85f42cb8f4
Naman Soni <namantsoni@gmail.com>
2025-01-28 13:12:02 +0530

    Syncing Clerk and Database

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
+import { currentUser } from "@clerk/nextjs/server";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
 import Link from "next/link";
+import { syncUser } from "@/actions/user.action";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const user = await currentUser();
+    if(user) await syncUser();
     return (
         <nav className="sticky top-0 left-0 w-full z-50 shadow border-b">
             <div className="max-w-7xl mx-auto px-4">
